4961871278c71fa16c519681220000ad9dc2afd5
twiecki <thomas.wiecki@gmail.com>
2014-03-30 18:42:18 +0900

    BUG: Remove p_stop from likelihood.

--- accumodel/models.py
+++ accumodel/models.py
@@ -103,7 +103,7 @@ class WaldAntiStop(WaldAntiPDA):
                                 ('t_anti', (0, .5)),
                                ])
     @staticmethod
-    def gen_data_anti(t=.3, a=2., v_pro=1., v_stop=1., v_anti=1., t_anti=1., p_stop=1):
+    def gen_data_anti(t=.3, a=2., v_pro=1., v_stop=1., v_anti=1., t_anti=1.):
         from scipy.stats import bernoulli
         if t < 0 or a < 0 or v_pro < 0 or v_anti < 0 or t_anti < 0:
             return None
@@ -112,9 +112,9 @@ class WaldAntiStop(WaldAntiPDA):
         x_pro = copy(func(t, a, v_pro, accum=0))
         x_anti = func(t + t_anti, a, v_anti, accum=1)
         x_stop = func(t, a, v_stop, accum=2)
-        if p_stop != 1:
-            stop = bernoulli(p_stop).rvs(x_stop.shape)
-            x_stop[np.logical_not(stop)] = np.inf
+        # if p_stop != 1:
+        #     stop = bernoulli(p_stop).rvs(x_stop.shape)
+        #     x_stop[np.logical_not(stop)] = np.inf
 
         x_pro[x_pro > x_stop] = np.inf
 
